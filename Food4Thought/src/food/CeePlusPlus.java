d5662ff12db03a51075c66c4fcffd233d886fffd
cao.dam <cao.dam@B204-18.soit.local>
2018-08-08 13:01:41 +1200

    CeePlusPlus completed

--- Food4Thought/src/food/CeePlusPlus.java
+++ Food4Thought/src/food/CeePlusPlus.java
@@ -1,32 +1,53 @@
 package food;
 
+
+
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.Font;
 import javax.swing.JButton;
 import javax.swing.JTextPane;
 import java.awt.Color;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Random;
 import java.awt.event.ActionEvent;
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
 
 public class CeePlusPlus extends JFrame {
+	
+	JFrame jframe;
 
 	private JPanel contentPane;
 	private JTextField textField;
 	private JTextField textField_1;
 	private JTextField textField_2;
+	
+	private static Connection myCon = null;
+	private static Statement stmt = null;
+	private static PreparedStatement preparedStmt = null;
+	private static ResultSet rs = null;
+
+	static final String dbUrl = "jdbc:mysql://localhost:3306/food";
+	static final String uname = "root";
+	static final String password = "";
 
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
+	public void CeePlusPlus() {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -44,7 +65,7 @@ public class CeePlusPlus extends JFrame {
 	 */
 	public CeePlusPlus() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 817, 424);
+		setBounds(100, 100, 940, 473);
 		contentPane = new JPanel();
 		contentPane.setBackground(Color.LIGHT_GRAY);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -56,9 +77,7 @@ public class CeePlusPlus extends JFrame {
 		lblNewLabel.setBounds(10, 0, 85, 30);
 		contentPane.add(lblNewLabel);
 		
-		JButton btnNewButton = new JButton("Place Order");
-		btnNewButton.setBounds(200, 340, 122, 23);
-		contentPane.add(btnNewButton);
+		
 		
 		JButton btnNewButton_1 = new JButton("Back");
 		btnNewButton_1.addActionListener(new ActionListener() {
@@ -121,7 +140,6 @@ public class CeePlusPlus extends JFrame {
 		label_9.setBounds(345, 315, 46, 14);
 		contentPane.add(label_9);
 		
-		
 		JLabel lblCustomerInformation = new JLabel("Customer Information");
 		lblCustomerInformation.setBounds(562, 27, 179, 14);
 		contentPane.add(lblCustomerInformation);
@@ -143,19 +161,20 @@ public class CeePlusPlus extends JFrame {
 		contentPane.add(lblNewLabel_2);
 		
 		textField = new JTextField();
-		textField.setBounds(512, 58, 191, 20);
+		textField.setBounds(504, 58, 193, 20);
 		contentPane.add(textField);
 		textField.setColumns(10);
 		
 		textField_1 = new JTextField();
 		textField_1.setColumns(10);
-		textField_1.setBounds(512, 94, 191, 20);
+		textField_1.setBounds(504, 94, 193, 20);
 		contentPane.add(textField_1);
 		
 		textField_2 = new JTextField();
 		textField_2.setColumns(10);
-		textField_2.setBounds(512, 136, 191, 20);
+		textField_2.setBounds(504, 136, 193, 20);
 		contentPane.add(textField_2);
+		
 		
 		JTextArea textArea = new JTextArea();
 		textArea.setBounds(403, 213, 294, 58);
@@ -283,7 +302,7 @@ public class CeePlusPlus extends JFrame {
 		contentPane.add(btnFood_4);
 		
 		JLabel lblPrice = new JLabel("Price ($):");
-		lblPrice.setBounds(276, 27, 59, 14);
+		lblPrice.setBounds(276, 27, 46, 14);
 		contentPane.add(lblPrice);
 		
 		
@@ -317,6 +336,74 @@ public class CeePlusPlus extends JFrame {
 		JLabel lblTotal = new JLabel("Total ($):");
 		lblTotal.setBounds(276, 315, 59, 14);
 		contentPane.add(lblTotal);
+		
+		
+		JButton btnNewButton = new JButton("Place Order");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if ( !textField.equals(null) && !textField_1.equals(null) && !textField_2.equals(null) && !textArea.equals(null)
+						) {
+
+					try {
+						myCon = DriverManager.getConnection(dbUrl, uname, password);
+						System.out.println("DB connection successful...");
+						
+						int chickenNum = Integer.parseInt(label.getText());
+						int chipsNum = Integer.parseInt(label_1.getText());
+						int fishNum = Integer.parseInt(label_2.getText());
+						int muffinNum = Integer.parseInt(label_3.getText());
+						int steakNum = Integer.parseInt(label_4.getText());
+						int priceTotal = Integer.parseInt(label_9.getText());
+						
+						String cLastName = textField.getText();
+						String cFirstName = textField_1.getText();
+						String cPhone = textField_2.getText();
+						String cAddress = textArea.getText();
+						
+						Random rand = new Random();
+						int  id = rand.nextInt(1000) + 1;
+						int minutes = rand.nextInt(60);
+						String time = Integer.toString(minutes) + "m";
+						
+						String sql = "insert into ceeplusplus" + " (orderID, chicken, chips, fish, muffin, steak, totalPrice, lastName, firstName, phone, address) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+						preparedStmt = myCon.prepareStatement(sql);
+						//set param values
+						preparedStmt.setInt(1, id);
+						preparedStmt.setInt(2, chickenNum);
+						preparedStmt.setInt(3, chipsNum);
+						preparedStmt.setInt(4, fishNum);
+						preparedStmt.setInt(5, muffinNum);
+						preparedStmt.setInt(6, steakNum);
+						preparedStmt.setInt(7, priceTotal);
+						preparedStmt.setString(8, cLastName);
+						preparedStmt.setString(9, cFirstName);
+						preparedStmt.setString(10, cPhone);
+						preparedStmt.setString(11, cAddress);
+																
+						preparedStmt.executeUpdate();						
+						preparedStmt.close();
+						myCon.close();
+						
+						JOptionPane.showMessageDialog(jframe, "Order Placed!\n" + "Total Price: "+  priceTotal + "$\n" + "Time estimated: " + time);
+									
+
+					} catch (Exception e1) {
+						e1.printStackTrace();
+					}
+				}
+				/*else if (!name.getText().matches("^[a-zA-Z]+$"))
+					JOptionPane.showMessageDialog(jframe, "Username cannnot be blank, contain numbers or symbols.",
+							"Oops!", JOptionPane.ERROR_MESSAGE);
+				else if (!passwordField.getText().matches("^^[a-zA-Z0-9]+$"))
+					JOptionPane.showMessageDialog(jframe, "Password cannnot be blank and contains no symbols.",
+							"Oops!", JOptionPane.ERROR_MESSAGE);*/
+				
+			}
+		});
+		btnNewButton.setBounds(200, 340, 122, 23);
+		contentPane.add(btnNewButton);
+		
 		
 		
 		
